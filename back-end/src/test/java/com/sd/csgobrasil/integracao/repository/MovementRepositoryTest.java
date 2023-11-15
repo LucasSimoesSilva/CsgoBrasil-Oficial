c3f1905f8c04f9289c44810ace84c7500770f7f8
LucasSimoesSilva <lucassimoes.carvalho@gmail.com>
2023-11-15 12:40:29 -0300

    Adicionando tests para o MovementRepository

--- back-end/src/test/java/com/sd/csgobrasil/integracao/repository/MovementRepositoryTest.java
+++ back-end/src/test/java/com/sd/csgobrasil/integracao/repository/MovementRepositoryTest.java
@@ -1,29 +1,58 @@
 package com.sd.csgobrasil.integracao.repository;
 
+import com.sd.csgobrasil.entity.DTO.Report;
 import com.sd.csgobrasil.repository.MovementRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
-@SpringBootTest
-public class MovementRepositoryTest {
+class MovementRepositoryTest {
 
     @Autowired
     MovementRepository repository;
 
     @Test
-    public void chamarMovement(){
+    void checkReportFromMovementReturnTheBuyerAndSellingStateTrue(){
+        List<String> nomeCompradorList = new ArrayList<>(List.of("Carlos","Administrador"));
+        List<String> nomeVendedorList = new ArrayList<>(List.of("EstoqueDinamico"));
 
+        List<Report> reports = repository.makeReport();
+
+        for (Report report : reports) {
+            if (report.getEstadoVenda()) {
+                assertTrue(nomeCompradorList.contains(report.getNomeComprador()));
+                assertTrue(nomeVendedorList.contains(report.getNomeVendedor()));
+            }
+        }
+    }
+
+    @Test
+    void checkReportFromMovementReturnNullToBuyerAndSellingStateFalse(){
+        List<String> nomeVendedorList = new ArrayList<>(List.of("EstoqueDinamico","EstoqueEstatico"
+                ,"Administrador","Carlos"));
+
+        List<Report> reports = repository.makeReport();
+
+        for (Report report : reports) {
+            if (!report.getEstadoVenda()) {
+                assertNull(report.getNomeComprador());
+                assertTrue(nomeVendedorList.contains(report.getNomeVendedor()));
+            }
+        }
     }
 
 
