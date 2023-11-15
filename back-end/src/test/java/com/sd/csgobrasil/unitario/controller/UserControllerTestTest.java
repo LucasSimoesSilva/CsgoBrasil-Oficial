5fe35bd7304425f82d227b5641ab159f5c31f24b
sam <samuelamorim811@gmail.com>
2023-11-15 13:51:55 -0300

    modificacoes de tipos

--- back-end/src/test/java/com/sd/csgobrasil/unitario/controller/UserControllerTestTest.java
+++ back-end/src/test/java/com/sd/csgobrasil/unitario/controller/UserControllerTestTest.java
@@ -1,20 +1,31 @@
 package com.sd.csgobrasil.unitario.controller;
 
+import com.sd.csgobrasil.service.UserSkinService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserControllerTestTest {
+public class UserControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private UserSkinService service;
+
+
+
     @Test
-    public void fazerCoisa(){
+    public void shouldReturnAlistOfSkinFromUser(){
+        Long idUser = 1L;
+
 
     }
 }
