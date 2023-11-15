8af14f1323692d6a503bcfbf61f230ac95b18350
LucasSimoesSilva <lucassimoes.carvalho@gmail.com>
2023-11-15 12:15:16 -0300

    Adicionando todos os testes do UserRepository

--- back-end/src/test/java/com/sd/csgobrasil/integracao/repository/MovementRepositoryTest.java
+++ back-end/src/test/java/com/sd/csgobrasil/integracao/repository/MovementRepositoryTest.java
@@ -1,16 +1,15 @@
 package com.sd.csgobrasil.integracao.repository;
 
-import com.sd.csgobrasil.entity.DTO.Report;
 import com.sd.csgobrasil.repository.MovementRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase
